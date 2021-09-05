6c728682111c113c36fda6f014072b40f7c1a1f4
0x78654C <l2xguard@live.com>
2021-09-05 23:39:59 +0300

    refactor

--- xBot_WPF/settings.xaml.cs
+++ xBot_WPF/settings.xaml.cs
@@ -6,6 +6,7 @@ using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using ModeratorManagement = xBot_WPF.Settings.ModeratorManagement;
 
 namespace xBot_WPF
 {
@@ -14,31 +15,39 @@ namespace xBot_WPF
     /// </summary>
     public partial class settings : Window
     {
-        //Declare global variables
-        private static string keyName = "xBot";
-        private static string t_userName;
-        private static string t_streamKey;
-        private static string weatherKey;
-        private static string apiKey;
-        private static string joinedKey;
-        private static string weatherUnits;
-        private static string ytRequest;
-        readonly static string modFile = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\data\mods.txt";
+        // Declare global variables
+        private readonly static string s_keyName = "xBot";
+        private static string s_twithcUserName;
+        private static string s_streamKey;
+        private static string s_weatherKey;
+        private static string s_apiKey;
+        private static string s_joinedKey;
+        private static string s_weatherUnits;
+        private static string s_youtubeRequest;
+        readonly static string s_modFile = Directory.GetCurrentDirectory() + @"\data\mods.txt";
         //---------------------------------------------------------
 
         public settings()
         {
             InitializeComponent();
-            //Load and display username, streamkey and dark mode control from registry
-            weatherUnits = Reg.regKey_Read(keyName, "weatherUnits");
-            t_userName = Reg.regKey_Read(keyName, "UserName");
-            t_streamKey = Reg.regKey_Read(keyName, "StreamKey");
-            if (t_streamKey != "")
+
+            ReadRegistryKeyVariables();
+            LoadSettings();
+            ModeratorManagement.LoadModerators(s_modFile,modsListV);
+        }
+
+        // Load data from Registry.
+        private void ReadRegistryKeyVariables()
+        {
+            s_weatherUnits = Reg.regKey_Read(s_keyName, "weatherUnits");
+            s_twithcUserName = Reg.regKey_Read(s_keyName, "UserName");
+            s_streamKey = Reg.regKey_Read(s_keyName, "StreamKey");
+            if (s_streamKey != "")
             {
                 try
                 {
 
-                    t_streamKey = Encryption._decryptData(Reg.regKey_Read(keyName, "StreamKey"));
+                    s_streamKey = Encryption._decryptData(Reg.regKey_Read(s_keyName, "StreamKey"));
                 }
                 catch (Exception e)
                 {
@@ -46,19 +55,22 @@ namespace xBot_WPF
                 }
             }
 
+            s_weatherKey = Reg.regKey_Read(s_keyName, "WeatherMSG");
+            s_apiKey = Reg.regKey_Read(s_keyName, "WeatherAPIKey");
+            s_joinedKey = Reg.regKey_Read(s_keyName, "BotMSG");
+            s_youtubeRequest = Reg.regKey_Read(s_keyName, "ytRequest");
 
-            weatherKey = Reg.regKey_Read(keyName, "WeatherMSG");
-            apiKey = Reg.regKey_Read(keyName, "WeatherAPIKey");
-            joinedKey = Reg.regKey_Read(keyName, "BotMSG");
-            ytRequest = Reg.regKey_Read(keyName, "ytRequest");
+            streamOauthKeyTXT.Password = s_streamKey;
+            userNameTXT.Text = s_twithcUserName;
+            weatherAPIKeyTXT.Password = Encryption._decryptData(s_apiKey);
+        }
 
-            streamOauthKeyTXT.Password = t_streamKey;
-            userNameTXT.Text = t_userName;
-            weatherAPIKeyTXT.Password = Encryption._decryptData(apiKey);
-            //---------------------------------
+        // Load settings and apply.
+        private void LoadSettings()
+        {
 
             //load yt song request setting
-            if (ytRequest == "1")
+            if (s_youtubeRequest == "1")
             {
                 songReqCKB.Content = "Activate YT Song Rquest: ON";
                 songReqCKB.IsChecked = true;
@@ -72,19 +84,19 @@ namespace xBot_WPF
 
             //load weaheter checkbox msg ar deactivate
 
-            if (weatherKey == "1")
+            if (s_weatherKey == "1")
             {
                 weatherCKB.Content = "Activate Weather Command: ON";
                 weatherCKB.IsChecked = true;
                 weaherUnits.IsEnabled = true;
             }
-            else if (weatherKey == "0")
+            else if (s_weatherKey == "0")
             {
                 weatherCKB.Content = "Activate Weather Command: OFF";
                 weatherCKB.IsChecked = false;
                 weaherUnits.IsEnabled = false;
             }
-            else if (weatherKey.Length <= 0)
+            else if (s_weatherKey.Length <= 0)
             {
                 weatherCKB.IsChecked = false;
                 weatherCKB.IsEnabled = false;
@@ -94,25 +106,25 @@ namespace xBot_WPF
 
             //load joiend checkbox msg ar deactivate
 
-            if (joinedKey == "1")
+            if (s_joinedKey == "1")
             {
-                joinedCKB.Content = "Display user joiend chat message: ON";
-                joinedCKB.IsChecked = true;
+                JoinedUserCheckBox.Content = "Display user joiend chat message: ON";
+                JoinedUserCheckBox.IsChecked = true;
             }
-            else if (joinedKey == "0")
+            else if (s_joinedKey == "0")
             {
-                joinedCKB.Content = "Display user joiend chat message: OFF";
-                joinedCKB.IsChecked = false;
+                JoinedUserCheckBox.Content = "Display user joiend chat message: OFF";
+                JoinedUserCheckBox.IsChecked = false;
             }
-            else if (joinedKey.Length <= 0)
+            else if (s_joinedKey.Length <= 0)
             {
-                joinedCKB.IsChecked = false;
-                joinedCKB.IsEnabled = false;
+                JoinedUserCheckBox.IsChecked = false;
+                JoinedUserCheckBox.IsEnabled = false;
             }
             //----------------------------------------
 
             //load weather units key control
-            if (weatherUnits == "1")
+            if (s_weatherUnits == "1")
             {
                 weaherUnits.SelectedIndex = 0;
             }
@@ -122,27 +134,8 @@ namespace xBot_WPF
             }
             //----------------------------------------
 
-            //Load mods list in listview
-            if (File.Exists(modFile))
-            {
-
-                //we add every mod from external file to listview
-                string[] bList = File.ReadAllLines(modFile);
-                foreach (var line in bList)
-                {
-                    if (line.Length > 0)
-                    {
-                        modsListV.Items.Add(line);
-                    }
-                }
-            }
-            else
-            {
-                MessageBox.Show("File " + modFile + " dose not exist!");
-            }
-            //-----------------------------------------
-
         }
+      
         /// <summary>
         /// Drag window with mouse
         /// </summary>
@@ -185,27 +178,27 @@ namespace xBot_WPF
         private void saveBTN_Click(object sender, RoutedEventArgs e)
         {
             //sotre username in regkey
-            Reg.regKey_WriteSubkey(keyName, "UserName", userNameTXT.Text);
+            Reg.regKey_WriteSubkey(s_keyName, "UserName", userNameTXT.Text);
 
             //sotre oauth key in regkey
             if (streamOauthKeyTXT.Password != "")
             {
-                Reg.regKey_WriteSubkey(keyName, "StreamKey", Encryption._encryptData(streamOauthKeyTXT.Password));
+                Reg.regKey_WriteSubkey(s_keyName, "StreamKey", Encryption._encryptData(streamOauthKeyTXT.Password));
             }
             else
             {
 
-                Reg.regKey_WriteSubkey(keyName, "StreamKey", streamOauthKeyTXT.Password);
+                Reg.regKey_WriteSubkey(s_keyName, "StreamKey", streamOauthKeyTXT.Password);
             }
 
             //sotre weather API key in regkey
             if (weatherAPIKeyTXT.Password != "")
             {
-                Reg.regKey_WriteSubkey(keyName, "WeatherAPIKey", Encryption._encryptData(weatherAPIKeyTXT.Password));
+                Reg.regKey_WriteSubkey(s_keyName, "WeatherAPIKey", Encryption._encryptData(weatherAPIKeyTXT.Password));
             }
             else
             {
-                Reg.regKey_WriteSubkey(keyName, "WeatherAPIKey", weatherAPIKeyTXT.Password);
+                Reg.regKey_WriteSubkey(s_keyName, "WeatherAPIKey", weatherAPIKeyTXT.Password);
             }
             MessageBox.Show("Settings saved!");
             this.Close();
@@ -215,7 +208,7 @@ namespace xBot_WPF
         private void weatherCKB_Checked(object sender, RoutedEventArgs e)
         {
             //Store the activation command control number
-            Reg.regKey_WriteSubkey(keyName, "WeatherMSG", "1");
+            Reg.regKey_WriteSubkey(s_keyName, "WeatherMSG", "1");
 
             //change the label content to on
             weatherCKB.Content = "Activate Weather Command: ON";
@@ -225,7 +218,7 @@ namespace xBot_WPF
         private void weatherCKB_Unchecked(object sender, RoutedEventArgs e)
         {
             //store the deactivation command control number
-            Reg.regKey_WriteSubkey(keyName, "WeatherMSG", "0");
+            Reg.regKey_WriteSubkey(s_keyName, "WeatherMSG", "0");
 
             //change the label content to off
             weatherCKB.Content = "Activate Weather Command: OFF";
@@ -234,37 +227,37 @@ namespace xBot_WPF
         #endregion
 
         #region saveing user joined keycontrol for main window check 
-        private void joinedCKB_Checked(object sender, RoutedEventArgs e)
+        private void JoinedUserCheckBox_Checked(object sender, RoutedEventArgs e)
         {
             //store the OnJoined event bot display message control number for activation
-            Reg.regKey_WriteSubkey(keyName, "BotMSG", "1");
+            Reg.regKey_WriteSubkey(s_keyName, "BotMSG", "1");
 
             //change the label content to on
-            joinedCKB.Content = "Display user joiend chat message: ON";
+            JoinedUserCheckBox.Content = "Display user joiend chat message: ON";
         }
 
-        private void joinedCKB_Unchecked(object sender, RoutedEventArgs e)
+        private void JoinedUserCheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
             //store the OnJoined event bot display message control number for deactivation
-            Reg.regKey_WriteSubkey(keyName, "BotMSG", "0");
+            Reg.regKey_WriteSubkey(s_keyName, "BotMSG", "0");
 
             //change the label content to on
-            joinedCKB.Content = "Display user joiend chat message: OFF";
+            JoinedUserCheckBox.Content = "Display user joiend chat message: OFF";
         }
         #endregion
 
         #region saveing yt song request settings
-        private void songReqCKB_Checked(object sender, RoutedEventArgs e)
+        private void SongRequestCheckBox_Checked(object sender, RoutedEventArgs e)
         {
 
-            Reg.regKey_WriteSubkey(keyName, "ytRequest", "1");
+            Reg.regKey_WriteSubkey(s_keyName, "ytRequest", "1");
             songReqCKB.Content = "Activate YT Song Rquest: ON";
         }
 
-        private void songReqCKB_Unchecked(object sender, RoutedEventArgs e)
+        private void SongRequestCheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
 
-            Reg.regKey_WriteSubkey(keyName, "ytRequest", "0");
+            Reg.regKey_WriteSubkey(s_keyName, "ytRequest", "0");
             songReqCKB.Content = "Activate YT Song Rquest: OFF";
         }
         #endregion
@@ -273,15 +266,15 @@ namespace xBot_WPF
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void weaherUnits_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void WeaherUnits_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (weaherUnits.SelectedItem.ToString().Contains("Celsius"))
             {
-                Reg.regKey_WriteSubkey(keyName, "weatherUnits", "1");
+                Reg.regKey_WriteSubkey(s_keyName, "weatherUnits", "1");
             }
             else
             {
-                Reg.regKey_WriteSubkey(keyName, "weatherUnits", "0");
+                Reg.regKey_WriteSubkey(s_keyName, "weatherUnits", "0");
             }
         }
 
@@ -291,31 +284,9 @@ namespace xBot_WPF
         /// <param name="sender"></param>
         /// <param name="e"></param>
 
-        private void addModBTN_Click(object sender, RoutedEventArgs e)
+        private void AddModeratorButton_Click(object sender, RoutedEventArgs e)
         {
-
-            if (File.Exists(modFile))
-            {
-
-                if (modNameTXT.Text.Length > 0)
-                {
-
-                    //   badWrodTXT.Text = Regex.Replace(badWrodTXT.Text, @"^\s+$[\r\n]*", string.Empty, RegexOptions.Multiline); //remove new line and emty line
-
-                    File.AppendAllText(modFile, modNameTXT.Text + Environment.NewLine);
-                    modsListV.Items.Add(modNameTXT.Text);
-                    modNameTXT.Clear();
-
-                }
-                else
-                {
-                    MessageBox.Show("You must fill the empy text box!");
-                }
-            }
-            else
-            {
-                MessageBox.Show("File " + modFile + " dose not exist!");
-            }
+            ModeratorManagement.AddModeratorToList(s_modFile, modNameTXT, modsListV);
         }
 
         /// <summary>
@@ -323,37 +294,9 @@ namespace xBot_WPF
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void delModBTN_Click(object sender, RoutedEventArgs e)
+        private void DeleteModeratorButton_Click(object sender, RoutedEventArgs e)
         {
-            if (File.Exists(modFile))
-            {
-                string line;
-                string bFile = File.ReadAllText(modFile);
-                if (modsListV.SelectedIndex == -1)
-                {
-                    MessageBox.Show("Please select a Moderator username for delete!");
-                }
-                else
-                {
-
-                    using (var sr = new StringReader(modsListV.SelectedItem.ToString()))
-                    {
-                        while ((line = sr.ReadLine()) != null)
-                        {
-                            bFile = bFile.Replace(line, "");
-                        }
-
-                        modsListV.Items.Remove(modsListV.SelectedItem.ToString());
-                        bFile = Regex.Replace(bFile, @"^\s+$[\r\n]*", string.Empty, RegexOptions.Multiline);//remove empty lines
-                        File.WriteAllText(modFile, bFile);
-
-                    }
-                }
-            }
-            else
-            {
-                MessageBox.Show("File " + modFile + " dose not exist!");
-            }
+            ModeratorManagement.RemoveSelectedModerator(s_modFile, modsListV);
         }
     }
 }
